ddebde32d7db5ecfa1eb26376fec12c917b46b9c
Moorea Tv <moorea@ymail.com>
2019-06-08 13:56:18 -0700

    woot, first working version
    need some changes to isboxer.SetMacro so it can be ran more than once
    /dbox m to resend mapping

--- DynamicBoxer/DynamicBoxer.lua
+++ DynamicBoxer/DynamicBoxer.lua
@@ -36,16 +36,64 @@ DB.Channel = string.gsub(select(2, BNGetInfo()), "#", "") -- also support multip
 -- or a StaticPopupDialogs / StaticPopup_Show
 DB.Secret = "PrototypeSecret12345"
 
-DB.debug = 1
+-- to force debugging even before saved vars are loaded
+-- DB.debug = 1
 
 DB.teamComplete = false
-DB.maxIter = 1
-DB.refresh = 5
+DB.maxIter = 3
+DB.refresh = 3
 
 DB.chatPrefix = "dbox0" -- protocol version in prefix
 DB.channelId = nil
 
 -- hook/replace isboxer functions by ours, keeping the original for post hook
+
+-- This event too early and the realm isn't available yet
+-- causing nil error trying to get the fully qualified name
+isboxer.frame:UnregisterEvent("UPDATE_BINDINGS")
+-- We need to fix isboxer.SetMacro so it can update buttons instead
+-- of leaking some/creating new ones each call:
+function isboxer.SetMacro(usename, key, macro, conditionalshift, conditionalalt, conditionalctrl, override)
+  if (key and key ~= "" and key ~= "none") then
+    local action = GetBindingAction(key)
+    if (action and action ~= "") then
+      -- TODO: only warn once and not each reconfiguration
+      if (not override) then
+        isboxer.Warning(key .. " is bound to " .. action .. ". ISBoxer is configured to NOT override this binding.")
+        return
+      else
+        isboxer.Warning(key .. " is bound to " .. action .. ". ISBoxer is overriding this binding.")
+      end
+    end
+
+    if (conditionalshift or conditionalalt or conditionalctrl) then
+      isboxer.CheckBoundModifiers(key, conditionalshift, conditionalalt, conditionalctrl)
+    end
+  end
+
+  local name
+  if (usename and usename ~= "") then
+    name = usename
+  else
+    name = "ISBoxerMacro" .. isboxer.NextButton
+  end
+  isboxer.NextButton = isboxer.NextButton + 1
+  local button
+  if _G[name] then
+    DB:Debug("Button for % already exist, reusing, setting macro to %", name, macro)
+    button = _G[name]
+  else
+    DB:Debug("Creating button %", name)
+    button = CreateFrame("Button", name, nil, "SecureActionButtonTemplate")
+  end
+  button:SetAttribute("type", "macro")
+  button:SetAttribute("macrotext", macro)
+  button:Hide()
+  if (key and key ~= "" and key ~= "none") then
+    SetOverrideBindingClick(isboxer.frame, false, key, name, "LeftButton")
+  end
+end
+
 DB.isbHooks = {LoadBinds = isboxer.LoadBinds, SetMacro = isboxer.SetMacro}
 
 function DB.LoadBinds()
@@ -57,13 +105,40 @@ function DB.LoadBinds()
   DB.isbHooks["LoadBinds"]()
 end
 
+function DB:Replace(macro)
+  self:Debug("macro before : %", macro)
+  local count = 0
+  for i, v in ipairs(self.Team) do
+    local o = v.orig
+    local n = v.new
+    -- TODO: probably should do this when setting the value instead of each time
+    local s, r = DB:SplitFullname(n)
+    if r == DB.myRealm then
+      n = s -- use the short name without realm when on same realm, using full name breaks (!)
+    end
+    local c
+    -- self:Debug("o=%, n=%", o, n)
+    macro, c = macro:gsub(o, n) -- will not work if one character is substring of another, eg foo and foobar
+    count = count + c
+  end
+  if count > 0 then
+    self:Debug("macro after %: %", count, macro)
+  end
+  return macro
+end
+
 function DB.SetMacro(username, key, macro, ...)
-  DB:Debug("Hooked SetMacro(%, %, %, %)", username, key, macro, DB.Dump(...))
+  -- DB:Debug("Hooked SetMacro(%, %, %, %)", username, key, macro, DB.Dump(...))
+  macro = DB:Replace(macro)
   DB.isbHooks["SetMacro"](username, key, macro, ...)
 end
 
 for k, v in pairs(DB.isbHooks) do
   isboxer[k] = DB[k]
+end
+
+function DB:SplitFullname(fullname)
+  return fullname:match("(.+)-(.+)")
 end
 
 -- Reverse engineer what isboxer will hopefully be providing more directly soon
@@ -74,15 +149,16 @@ function DB:ReconstructTeam()
     return
   end
   DB.fullName = DB:GetMyFQN()
+  DB.shortName, DB.myRealm = DB:SplitFullname(DB.fullName)
   local prev = isboxer.SetMacro
   DB.ISBTeam = {}
-  -- parse the text which looks like
-  -- "/assist [nomod:alt,mod:lshift,nomod:ctrl]CHAR1;[nomod:alt,mod:rshift,nomod:ctrl]CHAR2;[nomod:alt,nomod:shift,mod:lctrl]CHAR3;..."
+  -- parse the text which looks like (note the ]xxx; delimiters for most but \n at the end)
+  -- "/assist [nomod:alt,mod:lshift,nomod:ctrl]FIRST;[nomod:alt,mod:rshift,nomod:ctrl]SECOND;...[nomod:alt,mod:lshift,mod:lctrl]LAST\n""
   isboxer.SetMacro = function(macro, key, text)
     if macro ~= "FTLAssist" then
       return
     end
-    for x in text:gmatch("]([^;]+)[;\n]") do
+    for x in text:gmatch("%]([^;]+)[;\n]") do
       table.insert(DB.ISBTeam, x)
       if x == isboxer.Character.ActualName then
         DB.ISBIndex = #DB.ISBTeam
@@ -92,6 +168,8 @@ function DB:ReconstructTeam()
   isboxer.Character_LoadBinds()
   isboxer.SetMacro = prev
   DB:Debug("Found team to be % and my index % (while isb members is %)", DB.ISBTeam, DB.ISBIndex, isboxer.CharacterSet.Members)
+  DB.Team[DB.ISBIndex] = {orig = DB.ISBTeam[DB.ISBIndex], new = DB.fullName}
+  DB:Debug("Team map initial value = %", DB.Team)
 end
 
 function DB.Sync()
@@ -104,6 +182,9 @@ function DB.Sync()
   if not DB.channelId then
     DB.DynamicInit()
   end
+  if not DB.channelId then
+    return -- for now keep trying until we get a channel
+  end
   DB.maxIter = DB.maxIter - 1
   DB:Debug("Sync CB called for slot % our fullname is %, maxIter is now %", DB.ISBIndex, DB.fullName, DB.maxIter)
   if not DB.ISBIndex then
@@ -115,11 +196,42 @@ function DB.Sync()
   DB:Debug("Message success % on chanId %", ret, DB.channelId)
 end
 
+DB.Team = {}
+
+function DB:ProcessMessage(from, data)
+  local idxStr, realname, internalname = data:match("^([^ ]+) ([^ ]+) ([^ ]+)") -- or strplit(" ", data)
+  DB:Debug("from %, got idx=% realname=% internal name=%", from, idx, realname, internalname)
+  if from ~= realname then
+    DB:Debug("skipping unexpected mismatching name % != %", from, realname)
+  end
+  local idx = tonumber(idxStr)
+  if not idx then
+    DB:Debug("invalid non numerical idx %", idxStr)
+  end
+  if DB.Team[idx] and DB.Team[idx].new == realname then
+    DB:Debug("Already known mapping, skipping % %... team map is %", idx, realname, DB.Team)
+    return
+  end
+  DB.Team[idx] = {orig = internalname, new = realname}
+  DB:Debug("Team map is now %", DB.Team)
+  if EMAApi then
+    EMAApi.AddMember(realname)
+  end
+  isboxer.NextButton = 1 -- reset the buttons
+  DB.isbHooks["LoadBinds"]() -- maybe wait/batch/don't do it 5 times in small amount of time
+  DB:Print(DB.format("New mapping for slot %, dynamically set ISBoxer character to %", idx, realname), 0, 1, 1)
+end
+
 DB.EventD = {
 
   CHAT_MSG_ADDON = function(self, event, prefix, data, channel, sender, zoneChannelID, localID, name, instanceID)
     self:Debug("OnChatEvent called for % e=% channel=% p=% data=% from % z=%, lid=%, name=%, instance=%", self:GetName(), event,
                channel, prefix, data, sender, zoneChannelID, localID, name, instanceID)
+    if channel ~= "CHANNEL" or instanceID ~= DB.Channel then
+      self:Debug("wrong channel % or instance % vs %, skipping!", channel, instanceID, DB.Channel)
+      return -- not our message(s)
+    end
+    self:ProcessMessage(sender, data)
   end,
 
   PLAYER_ENTERING_WORLD = function(self, ...)
@@ -188,12 +300,14 @@ function DB.Join()
   local t, n = JoinTemporaryChannel(DB.Channel, DB.Secret)
   DB.channelId = GetChannelName(DB.Channel)
   DB:Debug("Joined channel % / % type % name % id %", DB.Channel, DB.Secret, t, n, DB.channelId)
+  DB:Print(DB.format("Joined DynBoxer secure channel. This is slot % and dynamically setting ISBoxer character to %", DB.ISBIndex,
+                     DB.fullName), 0, 1, 1)
   return DB.channelId
 end
 
 function DB.Help(msg)
   DB:Print("DynamicBoxer: " .. msg .. "\n" .. "/dbox c channel -- to change channel.\n" ..
-             "/dbox s secret -- to change the secret.\n" .. "/dbox join -- (re)join channel.\n" ..
+             "/dbox s secret -- to change the secret.\n" .. "/dbox m -- send mapping again\n" .. "/dbox join -- (re)join channel.\n" ..
              "/dbox debug on/off -- for debugging on or off.\n" .. "/dbox dump global -- to dump a global.")
 end
 
@@ -217,6 +331,10 @@ function DB.Slash(arg)
   if cmd == "j" then
     -- join
     DB.Join()
+  elseif cmd == "m" then
+    -- message again
+    DB.maxIter = 1
+    DB.Sync()
   elseif cmd == "c" then
     -- change channel 
     DB.SetSaved("Channel", rest)
@@ -249,10 +367,6 @@ DB:SetScript("OnEvent", DB.OnEvent)
 for k, _ in pairs(DB.EventD) do
   DB:RegisterEvent(k)
 end
-
--- This event too early and the realm isn't available yet
--- causing nil error trying to get the fully qualified name
-isboxer.frame:UnregisterEvent("UPDATE_BINDINGS")
 
 DB:Debug("dbox file loaded")
 DB.ticker = C_Timer.NewTicker(DB.refresh, DB.Sync)
