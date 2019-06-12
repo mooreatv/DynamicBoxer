65faf75c77e3e579ea3996a404569887f38650c2
Moorea Tv <moorea@ymail.com>
2019-06-12 09:52:47 -0700

    moved _ptr_ where it should have been

--- installdb.bat
+++ installdb.bat
@@ -1,5 +1,4 @@
-for %%x in (_retail_ _classic_beta_) do (
+for %%x in (_retail_ _classic_beta_ _ptr_) do (
 echo Installing for %%x
 xcopy /i /y DynamicBoxer\*.* "C:\Program Files (x86)\World of Warcraft\%%x\Interface\Addons\DynamicBoxer"
 )
-xcopy /i /y DynamicBoxer\*.* "C:\Program Files (x86)\World of Warcraft Public Test\_ptr_\Interface\Addons\DynamicBoxer"
