8516a7070b8abae118d805b7a6b065ee957f2119
Moorea Tv <moorea@ymail.com>
2019-06-18 16:46:27 -0700

    add _classic_ to the folder list

--- installdb.bat
+++ installdb.bat
@@ -1,4 +1,4 @@
-for %%x in (_retail_ _classic_beta_ _ptr_) do (
+for %%x in (_retail_ _classic_ _classic_beta_ _ptr_) do (
 echo Installing for %%x
 xcopy /i /y DynamicBoxer\*.* "C:\Program Files (x86)\World of Warcraft\%%x\Interface\Addons\DynamicBoxer"
 )
