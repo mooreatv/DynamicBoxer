4fb336d10a470d0a783b8cc370f53e6b587b6d89
Moorea Tv <moorea@ymail.com>
2019-09-12 20:41:20 -0700

    add auto label

--- classictag.sh
+++ classictag.sh
@@ -23,9 +23,9 @@ fi
 TOC_FILES=$(ls *.toc */*.toc 2> /dev/null)
 echo "Processing TOC file(s): $TOC_FILES"
 sed -i -e "s/## Interface:.*/## Interface: $CLASSIC_TOC_V/" $TOC_FILES
-git commit -a -m "Switching to classic toc $CLASSIC_TOC_V for $TAG-classic"
+git commit -a -m "[Auto] Switching to classic toc $CLASSIC_TOC_V for $TAG-classic"
 git tag ${TAG}-classic
 git push && git push --tags
 sed -i -e "s/## Interface:.*/## Interface: $RETAIL_TOC_V/" $TOC_FILES
-git commit -a -m "Switching back to retail toc $RETAIL_TOC_V"
+git commit -a -m "[Auto] Switching back to retail toc $RETAIL_TOC_V"
 git push
